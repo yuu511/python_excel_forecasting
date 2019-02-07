ff2599d94c4e5f197f12bce72462ba632a0f84bc
elijah <pipimi@tfwno.gf>
2019-02-07 04:40:32 -0800

    added a bunch of stuff lol

--- src/gen.py
+++ src/gen.py
@@ -6,9 +6,17 @@ from scipy import stats
 import sys
 import os
 import shutil
+import copy
 
 debug = False
 
+def graph_fcast_demand(fperiod,forecast,period,demand,name,path):
+  plt.plot (fperiod,forecast,period,demand)
+  plt.ylabel('demand')
+  plt.xlabel('period')
+  plt.savefig(os.path.join(path,name))
+  plt.close()
+  
 # generate error statistics (Error,ABS error, MSE , MAD, MAPE and TS)
 # based on forecast,error and demand, write on specified excel worksheet (w) beginning on (row,col)
 # NOTE: demand must equal the size of forecast for the calculation to be correct (Do array slicing BEFORE calling the function)
@@ -103,15 +111,22 @@ def calculate_error(fcast,demand,w,row,col):
 def static_forecast(xlsx,data,dirpath):
   static_forecast = xlsx.add_worksheet('static_foreast')
 
+  # our data 
+  period = data['period'].values
+  demand = data['demand'].values 
+  periodicity = data['periodicity'].values
+  num_demand = len(demand)
+  num_period = len(period)
+  p = int(periodicity[0])
+
   # init lists
-  period      = []
-  demand      = []
   des_x       = []
   des_demand  = []
   reg_demand  = []
   sea_factors = []
-  avg_sea     = []
-  fcast  = []
+  fcast       = []
+  avg_sea     = [0] * p
+  occurences_s= copy.deepcopy(avg_sea)
 
   # error statistics
   err = []
@@ -122,13 +137,6 @@ def static_forecast(xlsx,data,dirpath):
   mape = []
   ts = []
   
-  # our data 
-  period = data['period'].values
-  demand = data['demand'].values 
-  periodicity = data['periodicity'].values
-  num_demand = len(demand)
-  num_period = len(period)
-  p = int(periodicity[0])
   
   # column names
   c_names = ['Year','Period','Demand','De-Seasonalized Demand','Regressed,Deseasonalized Demand', 'Seasonal Factors', 'Average Seasonal Factors', 'Reintroduce Seasonality']
@@ -191,7 +199,7 @@ def static_forecast(xlsx,data,dirpath):
   slope, intercept, r_value, p_value, std_err = stats.linregress(des_x,des_demand)
   row = 1
   col = 4
-  for x in range (num_period):
+  for x in range (num_demand):
     reg = intercept + ((x+1) * (slope))
     reg_demand.append (reg)  
     static_forecast.write (row,col,reg)
@@ -209,16 +217,20 @@ def static_forecast(xlsx,data,dirpath):
   # average seasonal factors 
   row = 1
   col = 6
-  for x in range (p):
-    asea = (sea_factors[x] + sea_factors [x+p])/2
-    avg_sea.append (asea) 
-    static_forecast.write (row,col,asea)
+  for x in range (len(sea_factors)):
+    mod = x%p
+    avg_sea[mod]  += sea_factors[x] 
+    occurences_s[mod] += 1 
+
+  for x in range (len (avg_sea)):
+    avg_sea[x] = avg_sea[x] / occurences_s[x]
+    static_forecast.write (row,col,avg_sea[x])
     row += 1
   
   # reseasonalize demand
   row = 1
   col = 7
-  for x in range (num_period):
+  for x in range (num_demand):
     resea =  avg_sea[x%p] * reg_demand[x]
     fcast.append (resea)
     static_forecast.write (row,col,resea)
@@ -228,11 +240,7 @@ def static_forecast(xlsx,data,dirpath):
   err,aerr,mse,mad,perr,mape,ts = calculate_error(fcast,demand,static_forecast,1,8)
   
   # plot regressed, deseasonalized demand and reseasonalized demand
-  plt.plot (period,reg_demand,period,fcast)
-  plt.ylabel('demand')
-  plt.xlabel('period')
-  plt.savefig(os.path.join(dirpath,'static_forecast.png'))
-  plt.close()
+  graph_fcast_demand(period,demand,period,fcast,'static_forecast',dirpath)
 
   # debug print statements
   if (debug):
@@ -243,6 +251,9 @@ def static_forecast(xlsx,data,dirpath):
     print ("regressed_demand  %r " %  reg_demand )
     print ("avg_seasonal_factors     %r " %  avg_sea    )
     print ("reseasonalized_demand  %r " %  fcast )
+
+  # return linear regression of deseasonalized data and average seasonal factors for winter regression
+  return slope,intercept,avg_sea
 
 # calculate moving average forecast
 def moving_average(xlsx,data,dirpath):
@@ -311,11 +322,7 @@ def moving_average(xlsx,data,dirpath):
   err,aerr,mse,mad,perr,mape,ts = calculate_error(fcast,demand[p:len(demand)],moving_average,p+1,4)
 
   # plot the forecast
-  plt.plot (period, demand, period[len(period)-len(fcast):len(period)],fcast)
-  plt.savefig(os.path.join(dirpath,'moving_average_forecast.png'))
-  plt.ylabel('demand')
-  plt.xlabel('period')
-  plt.close()
+  graph_fcast_demand(period, demand, period[len(period)-len(fcast):len(period)],fcast,'moving_average_forecast',dirpath)
 
   # debug
   if (debug):
@@ -418,11 +425,238 @@ def simple_exponential_smoothing(xlsx,data,dirpath,alpha):
     print("ts   \n %r" % ts   )
 
   # plot simple exponential smoothing graph
-  plt.plot (period, demand, period,fcast)
-  plt.ylabel('demand')
-  plt.xlabel('period')
-  plt.savefig(os.path.join(dirpath,'exponential_smoothing_forecast.png'))
-  plt.close()
+  graph_fcast_demand(period,demand,period,fcast,'exponential_smoothing_forecast.png',dirpath)
+
+# calculate forecast with simple exponential smoothing
+def holt_trend_corrected_exponential_smoothing(xlsx,data,dirpath,alpha,beta):
+  ht = xlsx.add_worksheet('holt_trend_exp_smoothing')
+
+  # our data 
+  period = data['period'].values
+  demand = data['demand'].values 
+  periodicity = data['periodicity'].values
+  num_demand = len(demand)
+  num_period = len(period)
+  p = int(periodicity[0])
+
+  # init lists
+  lzero = None
+  tzero = None
+  lvl   = []
+  tnd   = []
+  fcast = []
+  err   = []
+  aerr  = []
+  mse   = []
+  mad   = []
+  perr  = []
+  mape  = []
+  ts    = []
+  
+  # column names
+  c_names = ['Period','Demand','Level','Trend','Forecast']
+  row = 0 
+  col = 0
+  for name in (c_names):
+    ht.write (row,col, name)
+    col += 1
+
+  slope, intercept, r_value, p_value, std_err = stats.linregress(period,demand)
+
+  # calculate Level L0
+  row = 1
+  col = 0
+  ht.write (row,col,0)
+  lzero = intercept
+  ht.write (row,col+2,lzero)
+  
+  # calculate Trend T0
+  row = 1
+  col = 3
+  tzero = slope
+  ht.write (row,col,slope)
+  
+  # fill out period
+  row = 2 
+  col = 0
+  for x in range (num_demand):
+    ht.write (row,col, x+1)
+    row += 1
+
+  # Demand Data
+  row = 2 
+  col = 1
+  for x in range (num_demand):
+    ht.write (row,col,demand[x]) 
+    row += 1
+
+  # Level + trend
+  row = 2 
+  col = 2
+  lvl.append(lzero)
+  tnd.append(tzero)
+  for x in range (num_demand):
+    level = (alpha * demand[x]) + ((1 - alpha) * (lvl[x]+tnd[x])) 
+    lvl.append(level)
+    trend = (beta * (lvl[x+1] - lvl [x])) + ((1 - beta) * tnd[x]) 
+    tnd.append(trend)
+    ht.write (row,col,level) 
+    ht.write (row,col+1,trend) 
+    row+=1
+ 
+  # Forecast
+  row = 2
+  col = 4
+  for x in range (len(lvl)-1):
+    forecast = lvl[x] + tnd[x]
+    fcast.append(forecast)
+    ht.write (row,col,forecast) 
+    row+=1
+ 
+  # write error column names and calculate error statistics 
+  err,aerr,mse,mad,perr,mape,ts = calculate_error(fcast,demand,ht,2,5)
+
+  # plot holt smoothing
+  graph_fcast_demand(period,demand,period,fcast,'holt_trend_correct_forecast.png',dirpath)
+
+  # debug
+  if (debug):
+    print("lvl  \n %r" % lvl  )
+    print("tnd  \n %r" % tnd  )
+    print("fcast\n %r" % fcast)
+    print("err  \n %r" % err  )
+    print("aerr \n %r" % aerr )
+    print("mse  \n %r" % mse  )
+    print("mad  \n %r" % mad  )
+    print("perr \n %r" % perr )
+    print("mape \n %r" % mape )
+    print("ts   \n %r" % ts   )
+
+# calculate forecast with simple exponential smoothing
+def winter_trend_seasonality_forecast(xlsx,data,dirpath,alpha,beta,gamma,slope,intercept,avg_sea):
+  wt = xlsx.add_worksheet('winter_trendseason')
+
+  # our data 
+  period = data['period'].values
+  demand = data['demand'].values 
+  periodicity = data['periodicity'].values
+  num_demand = len(demand)
+  num_period = len(period)
+  p = int(periodicity[0])
+
+  # init lists
+  lzero = None
+  tzero = None
+  sea   = []
+  lvl   = []
+  tnd   = []
+  fcast = []
+  err   = []
+  aerr  = []
+  mse   = []
+  mad   = []
+  perr  = []
+  mape  = []
+  ts    = []
+  fcasted = []
+  num_fcast = p
+   
+  # column names
+  c_names = ['Period','Demand','Level','Trend','Seasonal Factor','Forecast']
+  row = 0 
+  col = 0
+  for name in (c_names):
+    wt.write (row,col, name)
+    col += 1
+
+  # calculate Level L0 (Linregress of Deseasonalized data is calculated in static forecasting, transferred here)
+  row = 1
+  col = 0
+  wt.write (row,col,0)
+  lzero = intercept
+  wt.write (row,col+2,lzero)
+  
+  # calculate Trend T0 (Linregress of Deseasonalized data is calculated in static forecasting, transferred here)
+  row = 1
+  col = 3
+  tzero = slope
+  wt.write (row,col,tzero)
+  
+  # fill out period
+  row = 2 
+  col = 0
+  for x in range (num_demand+num_fcast):
+    wt.write (row,col, x+1)
+    row += 1
+
+  # Demand Data
+  row = 2 
+  col = 1
+  for x in range (num_demand):
+    wt.write (row,col,demand[x]) 
+    row += 1
+
+  for x in range (len(avg_sea)):
+    sea.append(avg_sea[x])  
+
+  # Level + trend + seasonal factor (up to number of data we have)
+  row = 2 
+  col = 2
+  lvl.append(lzero)
+  tnd.append(tzero)
+  for x in range (num_demand):
+    level = ((gamma* (demand[x]/sea[x]))) + ((1 - gamma) * (lvl[x]+tnd[x])) 
+    lvl.append (level)
+    wt.write (row,col,level) 
+    trend = ((alpha * (lvl[x+1] - lvl [x])) + ((1- alpha)*tnd[x])) 
+    tnd.append (trend)
+    wt.write (row,col+1,trend) 
+    seasonalf = ((alpha * (demand[x]/lvl[x+1])) + ((1 - alpha)*sea[x]))
+    wt.write (row,col+2,seasonalf) 
+    sea.append (seasonalf)
+    row += 1
+
+  # generate forecast (up to demand data length)
+  row = 2
+  col = 5
+  for x in range (num_demand):
+    forecast =  (lvl[x] + tnd[x]) * sea[x]
+    fcast.append(forecast)
+    wt.write (row,col,forecast)
+    row += 1
+  
+  # forecast predict ( up to demand length + p in this case ) 
+  row = num_demand+2
+  col = 4
+  for x in range (num_demand,num_demand+num_fcast):
+    period = np.concatenate([period,[x+1]])
+    predict = (lvl [num_demand-1] + ((x-num_demand) * tnd[num_demand-1]))* sea [x] 
+    fcast.append(predict)
+    fcasted.append(predict)
+    wt.write (row,col,sea[x]) 
+    wt.write (row,col+1,predict) 
+    row +=1
+
+  # write error column names and calculate error statistics 
+  err,aerr,mse,mad,perr,mape,ts = calculate_error(fcast[0:num_demand],demand,wt,2,6)
+
+  # plot winter smoothing
+  graph_fcast_demand(period[0:num_demand],demand,period,fcast,'winter_trend_season_forecast.png',dirpath)
+
+  print (period)
+  print (fcast)
+  # debug
+  if (debug):
+    print("lvl  \n %r" % lvl  )
+    print("tnd  \n %r" % tnd  )
+    print("fcast\n %r" % fcast)
+    print("err  \n %r" % err  )
+    print("aerr \n %r" % aerr )
+    print("mse  \n %r" % mse  )
+    print("mad  \n %r" % mad  )
+    print("perr \n %r" % perr )
+    print("mape \n %r" % mape )
+    print("ts   \n %r" % ts   )
 
 if __name__ == "__main__":
   src_path = os.path.abspath(__file__)
@@ -486,14 +720,25 @@ if __name__ == "__main__":
   if not ('periodicity' in data):
     print ("no periodicity data")
     sys.exit(1)
+  periodicity = data['periodicity'].values
+  if not (isinstance(periodicity[0],float)): 
+    print ("no periodicity data")
+    sys.exit(1)
 
   # make graph directory to store all graph pictures in
   graphpath = os.path.abspath(os.path.join(dirname,'graphs'))
   os.mkdir(graphpath)
   dirname   = os.path.abspath(dirname)
   xlsx = xlsxwriter.Workbook(os.path.join(dirname,'generated_spreadsheet.xlsx'))
-  static_forecast(xlsx,data,graphpath)
-  moving_average(xlsx,data,graphpath)
   alpha = 0.1
+  beta = 0.2
+  gamma = 0.05
+
+  # make the forecasts
+  # return slope,intercept of regression and seasonal factors of deseasonalized demand for winter forecasting
+  slope, intercept, avg_sea = static_forecast(xlsx,data,graphpath)
+  moving_average(xlsx,data,graphpath)
   simple_exponential_smoothing(xlsx,data,graphpath,alpha)
+  holt_trend_corrected_exponential_smoothing(xlsx,data,graphpath,alpha,beta)
+  winter_trend_seasonality_forecast(xlsx,data,graphpath,alpha,beta,gamma,slope,intercept,avg_sea)
   xlsx.close()
